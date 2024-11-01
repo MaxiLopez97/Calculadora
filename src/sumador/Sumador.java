477d6565e983c1eb2b1e760194946a1c7096c307
MaxiLopez97 <142050147+MaxiLopez97@users.noreply.github.com>
2024-11-01 12:50:17 -0300

    Initial commit

---
+++ src/sumador/Sumador.java
@@ -0,0 +1,17 @@
+
+package sumador;
+
+/**
+ *
+ * @author Maxi
+ */
+public class Sumador {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
